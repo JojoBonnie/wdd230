1a7876894c0e49eb147a9a33ab749ba8da976a44
jojobonnie <jmasebe@gmail.com>
2024-01-14 07:52:30 +0200

    W01-home page

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+// getDates.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Populate current year
+    document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+    // Populate last modified date
+    document.getElementById('lastModified').textContent = 'Last Modified: ' + new Date(document.lastModified).toUTCString();
+});
