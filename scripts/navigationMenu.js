200fb81c16728c61efd5e1b5588f14c1e0254ce6
jojobonnie <jmasebe@gmail.com>
2024-02-18 07:40:54 +0200

    w3-submission

--- scripts/navigationMenu.js
+++ scripts/navigationMenu.js
@@ -1,30 +1,7 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+function toggleMenu() {
+    var navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('active');
+    var menuIcon = document.getElementById('menu');
+    menuIcon.textContent = menuIcon.textContent === '☰' ? '✖' : '☰'; // Toggle icons
+}
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
-
-const links = document.querySelectorAll('.navigation li');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        links.forEach(link => link.classList.remove('active'));
-        link.classList.add('active');
-    });
-});
-
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("🌙")) {
-		main.classList.add("dark");
-        main.querySelectorAll("section").forEach(section => section.classList.add("dark"));
-		modeButton.textContent = "🔆";
-	} else {
-		main.classList.remove("dark");
-        main.querySelectorAll("section").forEach(section => section.classList.remove("dark"));
-		modeButton.textContent = "🌙";
-	}
-});
