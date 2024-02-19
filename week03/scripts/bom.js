9b750eb1c747bea9cfa60ad7a32d03ae75bf57f2
jojobonnie <jmasebe@gmail.com>
2024-02-19 23:15:12 +0200

    w3_bom_v2

--- week03/scripts/bom.js
+++ week03/scripts/bom.js
@@ -1,10 +1,10 @@
-/*
-declare three const variables that hold references to the input, button, and .list elements.
-*/
+
+// declare three const variables that hold references to the input, button, and .list elements.
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
+/*
 button.addEventListener('click', () => {
     if (input.value !== '') {
         const li = document.createElement('li');
@@ -22,3 +22,65 @@ button.addEventListener('click', () => {
         input.focus();
     }
 });
+
+*/
+
+// Declare and Initialize chaptersArray
+let chaptersArray = getChapterList() || [];
+
+// Populate Displayed List
+chaptersArray.forEach(chapter => {
+  displayList(chapter);
+});
+
+// Button Click Event Listener
+button.addEventListener('click', () => {
+  if (input.value !== '') {
+    displayList(input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
+    input.value = '';
+    input.focus();
+  }
+  else {
+    input.focus();
+  }
+});
+
+// Define displayList Function
+function displayList(item) {
+  // Create list item and append to the list
+  const listItem = document.createElement('li');
+  listItem.textContent = item;
+
+  // Add delete button to each list item
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = '❌';
+  deleteButton.addEventListener('click', () => {
+    deleteChapter(item);
+    listItem.remove();
+  });
+
+  // Append delete button to the list item
+  listItem.appendChild(deleteButton);
+
+  // Append the list item to the list
+  list.appendChild(listItem);
+}
+
+// Define setChapterList Function
+function setChapterList() {
+  localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+// Define getChapterList Function
+function getChapterList() {
+  return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+// Define deleteChapter Function
+function deleteChapter(chapter) {
+  chaptersArray = chaptersArray.filter(item => item !== chapter);
+  setChapterList();
+}
+
