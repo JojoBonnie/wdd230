bce1b9f8cadb9331a2fae645f4357e55b5434cc2
jojobonnie <jmasebe@gmail.com>
2024-02-19 10:43:10 +0200

    W02_submission

--- scripts/darkModeToggle.js
+++ scripts/darkModeToggle.js
@@ -1,6 +1,6 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
-    main.querySelectorAll("section").forEach(section => section.classList.toggle('dark-mode'));
+    document.querySelectorAll("section").forEach(section => section.classList.toggle('dark-mode'));
     var modeIcon = document.getElementById('mode');
     modeIcon.textContent = modeIcon.textContent === '🌙' ? '☀️' : '🌙'; // Toggle icons
 }
