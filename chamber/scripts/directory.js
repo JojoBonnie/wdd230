ae9fd36d9b55e4ef8f713a83672f33b67e3f5888
jojobonnie <jmasebe@gmail.com>
2024-02-21 00:02:27 +0200

    chamber-directory

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const viewToggle = document.getElementById('viewToggle');
+    const membersContainer = document.getElementById('members-container');
+
+    fetch('data/members.json')
+        .then(response => response.json())
+        .then(data => {
+            displayMembers(data.members);
+            toggleView(); // Set the initial view
+        });
+
+    viewToggle.addEventListener('change', toggleView);
+    console.log('Directory page is loaded');
+});
+
+function displayMembers(members) {
+    const membersContainer = document.getElementById('members-container');
+
+    membersContainer.innerHTML = '';
+        members.forEach(member => {
+            const memberCard = document.createElement('div');
+            memberCard.className = 'card'; // Use 'card' instead of 'row' for consistency
+
+            memberCard.innerHTML = `
+                <img src="images/${member.image}" alt="${member.name} Logo">
+                <h3>${member.name}</h3>
+                <p>${member.address}</p>
+                <p>Phone: ${member.phone}</p>
+                <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+                <p>Membership Level: ${member.membershipLevel}</p>
+                <p>${member.otherInfo}</p>
+            `;
+            membersContainer.appendChild(memberCard);
+        });
+
+}
+
+function toggleView() {
+    const viewToggle = document.getElementById('viewToggle');
+    const membersContainer = document.getElementById('members-container');
+    const containers = membersContainer.querySelectorAll('div');
+    containers.forEach(memberContainer => {
+        memberContainer.classList.toggle('card', viewToggle.options[viewToggle.selectedIndex].value === 'grid');
+        memberContainer.classList.toggle('row', viewToggle.options[viewToggle.selectedIndex].value === 'list');
+    });
+    membersContainer.classList.toggle('grid-view', viewToggle.options[viewToggle.selectedIndex].value === 'grid');
+    membersContainer.classList.toggle('list-view', viewToggle.options[viewToggle.selectedIndex].value === 'list');
+    
+}
--- scripts/darkModeToggle.js
+++ scripts/darkModeToggle.js
@@ -1,6 +1,7 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
     document.querySelectorAll("section").forEach(section => section.classList.toggle('dark-mode'));
+    document.querySelectorAll("div").forEach(div => div.classList.toggle('dark-mode'));
     var modeIcon = document.getElementById('mode');
     modeIcon.textContent = modeIcon.textContent === '🌙' ? '☀️' : '🌙'; // Toggle icons
 }
